a209cbd152305daaf0ac389a62850a51c1d4e79a
MrPoP <modyali_fox@yahoo.com>
2023-02-23 16:10:59 -0800

    fixing error 24 lines of code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,7 +6,9 @@
 int main(void)
 {
 	int i;
+
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
 /*
 *	while (i < 10)
@@ -14,6 +16,9 @@ int main(void)
 *		putchar(i);
 *	}
 */
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
+
 }
